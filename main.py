90fc6989cbf83798ce43cdcbb9e68cd399fe538a
Дмитрий <pii.82@mail.ru>
2024-11-01 16:53:47 +0700

    Добавлена кнопка удаления

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from locale import currency
-
 from opencage.geocoder import OpenCageGeocode
 from tkinter import *
 import webbrowser
 
-from tornado.gen import Return
 
 
 def get_coordinates(city, key):
@@ -15,12 +12,13 @@ def get_coordinates(city, key):
             lat = round(results[0]['geometry']['lat'], 2)
             lon = round(results[0]['geometry']['lng'], 2)
             country = results[0]['components']['country']
+            name = results[0]['annotations']['currency']['name']
             osm_url = f"https://www.openstreetmap.org/?mlat={lat}&mlon={lon}"
 
             if 'state' in results[0]['components']:
                 region = results[0]['components']['state']
                 return {
-                "coordinates": f"Широта: {lat}, Долгота: {lon} \n Страна: {country}.\n Регион: {region}.\nВалюта: {currency}.",
+                "coordinates": f"Широта: {lat}, Долгота: {lon} \n Страна: {country}.\n Регион: {region}. \n Валюта: {name}",
                 "map_url": osm_url
                         }
             else:
@@ -46,12 +44,16 @@ def show_map():
         webbrowser.open(map_url)
 
 
+def delete():
+    entry.delete(0, END,)
+
+
 key = "8f34e7c28e0d43b19f20c8fc0d09e4e9"
 map_url = ""
 
 window=Tk()
 window.title("Координаты городов")
-window.geometry("350x160")
+window.geometry("350x190")
 
 entry = Entry()
 entry.pack()
@@ -65,5 +67,8 @@ label.pack()
 
 map_button = Button(text="Показать карту", command=show_map)
 map_button.pack()
+
+button = Button(text="Очистить", command=delete)
+button.pack()
 
 window.mainloop()
